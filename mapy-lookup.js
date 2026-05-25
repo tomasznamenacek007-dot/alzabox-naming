211d2ad4f260b6d557ccb20274ae476439c2f51b
tomasznamenacek007-dot <tomas.znamenacek007@gmail.com>
2026-05-25 15:02:24 +0200

    Update mapy-lookup.js

--- mapy-lookup.js
+++ mapy-lookup.js
@@ -1,3 +1,24 @@
-export default function handler(req, res) {
-  res.status(200).json({ ok: true, message: "Mapy endpoint funguje" })
+export default async function handler(req, res) {
+  const { gps } = req.query
+
+  if (!gps) {
+    return res.status(400).json({ error: 'Chybí GPS' })
+  }
+
+  try {
+    const response = await fetch(
+      `https://api.mapy.cz/v1/rgeocode?lon=${gps.split(',')[1]}&lat=${gps.split(',')[0]}`,
+      {
+        headers: {
+          'X-Mapy-Api-Key': process.env.VITE_MAPY_API_KEY
+        }
+      }
+    )
+
+    const data = await response.json()
+
+    res.status(200).json(data)
+  } catch (err) {
+    res.status(500).json({ error: 'Chyba API' })
+  }
 }
